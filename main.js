937faf39aba2b0951e2671b900abba2d7e3098b0
theimaginedseven <103391499+theimaginedseven@users.noreply.github.com>
2022-12-28 09:43:28 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function draw() {
+    background('#b5adad');
+    textSize(difference);
+    fill('#00f5ff');
+    text('Jakob', 200, 70);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
